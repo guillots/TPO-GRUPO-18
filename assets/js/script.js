d8360832cdd9c7ad423d04803c2a84fe73c2ba93
Guillermo Lovento <cglovento@gmail.com>
2023-05-18 17:01:23 -0300

    agregado de API REST

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,19 @@ $(document).ready(function() {
 
     });
 });
+
+
+var form = document.getElementById('contact-form');
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("contact-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      form.reset();
+      alert('Mensaje enviado. Nos contactaremos a la brevedad')
+    });
+  });
