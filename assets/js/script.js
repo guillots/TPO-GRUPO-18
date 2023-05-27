ba166fb0e2841244970884f12012b3b2966a9b64
Guillermo Lovento <cglovento@gmail.com>
2023-05-27 19:20:49 -0300

    actualizacion y acomodo de header y modificacion de footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-// codigo anterior JS para navbar
-// $(document).ready(function() {
-//     var altura = $('.menu').offset().top;
-    
-//     $(window).on('scroll', function(){
-//         if ($ (window).scrollTop () > altura){
-//             $('.menu').addClass('menu-fixed');
-//         } else {
-//             $('.menu').removeClass('menu-fixed');
-//         }
-
-//     });
-// });
-
-// Codigo para dejar navbar arriba
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
